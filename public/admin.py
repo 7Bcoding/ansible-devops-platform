b767dd0a909e081ee7c1359dd3504d244e3ce065
7Bcoding <541965325@qq.com>
2020-08-10 11:16:34 +0800

    20200810更新，admin平台从数据库获取主机和组关系，保存至主机清单

--- public/admin.py
+++ public/admin.py
@@ -1,46 +1,49 @@
-
 from django.contrib import admin
 from public.models import *
+# inventory 对应文件
+from tools.config import inventory
+
 
 @admin.register(AnsiblePlaybooks)
 class AnsiblePlaybooksAdmin(admin.ModelAdmin):
     list_display = ['nickName', 'playbook']
 
-@admin.register(AnsibleTasks)    #
+
+@admin.register(AnsibleTasks)  #
 class AnsibleTasksAdmin(admin.ModelAdmin):
     list_display = [
-            'AnsibleID',
-            'CeleryID',
-            'GroupName',
-            'playbook',
-            'ExtraVars',
-            'AnsibleResult',
-            'CeleryResult',
-            'CreateTime'
-            ]
+        'AnsibleID',
+        'CeleryID',
+        'GroupName',
+        'playbook',
+        'ExtraVars',
+        'AnsibleResult',
+        'CeleryResult',
+        'CreateTime'
+    ]
 
-# inventory 对应文件
-from tools.config import inventory
 
 # 主机列表修改内容
 @admin.register(Hosts)
 class HostsAdmin(admin.ModelAdmin):
     list_display = ['hostname', 'hostip']
 
+
 # 组列表相关内容
 @admin.register(Groups)
 class GroupsAdmin(admin.ModelAdmin):
     list_display = ['groupName']
     filter_horizontal = ('hostList',)
+
     # 重写 save_related 函数
     def save_related(self, request, form, formsets, change):
         super().save_related(request, form, formsets, change)
-        # 在 admin 平台保存完对应关系之后，获取所有对应关系，保存到对应文件中
+        # 在 admin 平台保存完对应关系之后，获取所有对应关系，保存到主机清单中
         if change:
             data = "\n"
             gs = Groups.objects.all()
             for g in gs:
                 data += '[%s]\n' % g.groupName
-                data += '\n'.join([ i[0] for i in g.hostList.values_list('hostip') ])
+                data += '\n'.join([i[0] for i in g.hostList.values_list('hostip')])
             with open(inventory, 'w') as f:
                 f.write(data)
