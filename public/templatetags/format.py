c7331601f97651e06ba89a7c5436d058f3a56a91
7Bcoding <541965325@qq.com>
2020-08-10 11:14:14 +0800

    20200810更新，自定义过滤器，格式化Celery结果

--- public/templatetags/format.py
+++ public/templatetags/format.py
@@ -6,20 +6,21 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()  # 自定义filter时必须加上
 
+
 # 我们专门定义了一个函数处理 celery 结果
 @register.filter(is_safe=True)  # 注册template filter
 @stringfilter  # 希望字符串作为参数
-def celery_status(value):   # 格式化 celery
+def celery_status(value):  # 格式化 celery
     if value:
         d = json.loads(value)
         s = d.get('status')
     else:
         s = 'RUNNING'
     r = {
-            'FAILURE': '<p style="color:red">失败</p>',
-            'SUCCESS': '<p style="color:blue">完成</p>',
-            'RUNNING': '<p style="color:#d600ff">执行中</p>'
-        }
+        'FAILURE': '<p style="color:red">失败</p>',
+        'SUCCESS': '<p style="color:blue">完成</p>',
+        'RUNNING': '<p style="color:#d600ff">执行中</p>'
+    }
     return mark_safe(r.get(s))
 
 
@@ -27,26 +28,28 @@ def celery_status(value):   # 格式化 celery
 # 对 ansible 结果的分析是一个复杂的事情，有可能需要针对业务环境进行具体开发
 @register.filter(is_safe=True)
 @stringfilter
-def ansible_result(s):      #ansible_result
+def ansible_result(s):  # ansible_result
     if not s:
         return "未搜索🔍到结果"
     data = json.loads(s)
     msg = ""
     for d in data:
-        if  d.get('status') in [ "failed", "unreachable" ]:
+        if d.get('status') in ["failed", "unreachable"]:
             msg += '<span style="color:red">{host} | {task} => {status}<br>        {msg}</span><br>'.format(
                 host=d['host'], task=d['task'], status=d['status'], msg=d['result']['msg']
             )
             continue
-        elif d['result']['changed'] == False and d['status'] != 'ignoring':
+        elif d['result']['changed'] is False and d['status'] != 'ignoring':
             color = 'green'
-        elif d['result']['changed'] == False:
+        elif not d['result']['changed']:
             color = 'red'
-        elif d['result']['changed'] == True:
+        elif d['result']['changed']:
             color = 'yellow'
-        msg += '''<span style="color:{color}">{host} | {task} => {status} <br>        "changed": {changed}, <br>        "{task}": {data} </span><br>'''.format(
-                color=color, host=d['host'], task=d['task'], status=d.get('status', 'None'),data=d['result'].get('msg', ''), changed=d['result'].get('changed')
-            )
+        msg += '''<span style="color:{color}">{host} | {task} => {status} <br>        "changed": {changed}, 
+        <br>        "{task}": {data} </span><br>'''.format(
+            color=color, host=d['host'], task=d['task'], status=d.get('status', 'None'),
+            data=d['result'].get('msg', ''), changed=d['result'].get('changed')
+        )
         if d['status'] == 'skipped':
             msg += '<span style="color:rebeccapurple">......%s     [%s]</span><br>' % ('跳过上个任务', d['host'])
         elif d['status'] == 'ignoring':
